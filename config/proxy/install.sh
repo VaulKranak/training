7a024218b41b88e19a588c12e13ed6672806adcf
vaulkranak <vaulx.kranak@intel.com>
2018-09-19 10:23:25 -0700

    fixed cp loc

--- config/proxy/install.sh
+++ config/proxy/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+sudo [ -d ~/.docker ] && echo ~/.docker exists || mkdir ~/.docker
 sudo cp config.json ~/.docker/config.json
-sudo cp docker /etcdefault/docker
-sudo cp docker.service /libsystemd/system/docker.service
-sudo cp /etc/NetworkManager/NetworkManager.conf
+sudo cp docker /etc/default/docker
+sudo cp docker.service /lib/systemd/system/docker.service
+sudo cp /etc/NetworkManager/NetworkManager.conf NetworkManager.conf
 sudo systemctl daemon-reload
 sudo systemctl restart docker
 sudo systemctl restart network-manager
-
