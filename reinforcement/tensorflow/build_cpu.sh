a113dd577fa821eba60b06995b514cb093e3e7de
vaulkranak <vaulx.kranak@intel.com>
2018-09-19 11:57:14 -0700

    added :

--- reinforcement/tensorflow/build_cpu.sh
+++ reinforcement/tensorflow/build_cpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sed -i "s/.TERMINATION_ACCURACY.: ..../\"TERMINATION_ACCURACY\": ${1:-0.03}/g" minigo/params/final.json 
-sed -i "s/\"NUM_PARALLEL_SELFPLAY\":.*,/\"NUM_PARALLEL_SELFPLAY\" ${2:-16},/g" minigo/params/final.json 
+sed -i "s/\"NUM_PARALLEL_SELFPLAY\":.*,/\"NUM_PARALLEL_SELFPLAY\": ${2:-16},/g" minigo/params/final.json 
 sed -i "s/install tensorflow*/install tensorflow/g" Dockerfile
 
 
