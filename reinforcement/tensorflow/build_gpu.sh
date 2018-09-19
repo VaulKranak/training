50e664b5f62ad1c28aeabd0d29044205cf1b5e60
vaulkranak <vaulx.kranak@intel.com>
2018-09-19 15:21:29 -0700

    fixed dockerfile added gpu script

---
+++ reinforcement/tensorflow/build_gpu.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sed -i "s/.TERMINATION_ACCURACY.: ..../\"TERMINATION_ACCURACY\": ${1:-0.03}/g" minigo/params/final.json 
+sed -i "s/\"NUM_PARALLEL_SELFPLAY\":.*,/\"NUM_PARALLEL_SELFPLAY\": ${2:-16},/g" minigo/params/final.json 
+sed -i "s/install tensorflow.*/install tensorflow-gpu/g" Dockerfile
+
+
+
+IMAGE=`sudo docker build . | tail -n 1 | awk '{print $3}'`
+SEED=1
+NOW=`date "+%F-%T"`
+sudo docker run --runtime=nvidia -t -i $IMAGE "./run_and_time.sh" $SEED | tee benchmark-$NOW.log
