2952f57a2f94ccf30fdc1edb334e232ca8417ac3
vaulkranak <vaulx.kranak@intel.com>
2018-09-20 09:47:24 -0700

    fixed paths

--- config/proxy/install.sh
+++ config/proxy/install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+script_dir=$(dirname $0)
 
 sudo [ -d ~/.docker ] && echo ~/.docker exists || mkdir ~/.docker
-sudo cp config.json ~/.docker/config.json
-sudo cp docker /etc/default/docker
-sudo cp docker.service /lib/systemd/system/docker.service
-sudo cp /etc/NetworkManager/NetworkManager.conf NetworkManager.conf
+sudo cp $script_dir/config.json ~/.docker/config.json
+sudo cp $script_dir/docker /etc/default/docker
+sudo cp $script_dir/docker.service /lib/systemd/system/docker.service
+sudo cp $script_dir/NetworkManager.conf /etc/NetworkManager/NetworkManager.conf
 sudo systemctl daemon-reload
 sudo systemctl restart docker
 sudo systemctl restart network-manager
