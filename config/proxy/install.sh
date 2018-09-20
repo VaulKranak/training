7fcbb00b279f9b4213f4dd3509bd7b34e1fc8976
vaulkranak <vaulx.kranak@intel.com>
2018-09-20 10:42:38 -0700

    added missing service file

--- config/proxy/install.sh
+++ config/proxy/install.sh
@@ -5,6 +5,8 @@ sudo [ -d ~/.docker ] && echo ~/.docker exists || mkdir ~/.docker
 sudo cp $script_dir/config.json ~/.docker/config.json
 sudo cp $script_dir/docker /etc/default/docker
 sudo cp $script_dir/docker.service /lib/systemd/system/docker.service
+sudo [ -d /etc/systemd/system/docker.service.d ] && echo /etc/systemd/system/docker.service.d exists || sudo mkdir /etc/systemd/system/docker.service.d
+sudo cp $script_dir/http-proxy.conf /etc/systemd/system/docker.service.d/http-proxy.conf
 sudo cp $script_dir/NetworkManager.conf /etc/NetworkManager/NetworkManager.conf
 sudo systemctl daemon-reload
 sudo systemctl restart docker
