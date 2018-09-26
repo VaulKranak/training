0d23543f9cb6461b8d1c47de01d3ad8bd3e6c431
VaulKranak <vaulx.kranak@intel.com>
2018-09-26 13:46:57 -0700

    :(

--- config/specific/tensorrt/install.sh
+++ config/specific/tensorrt/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo wget https://developer.nvidia.com/compute/machine-learning/tensorrt/5.0/rc/nv-tensorrt-repo-ubuntu1604-cuda9.0-trt5.0.0.10-rc-20180906_1-1_amd64
+#sudo wget https://developer.nvidia.com/compute/machine-learning/tensorrt/5.0/rc/nv-tensorrt-repo-ubuntu1604-cuda9.0-trt5.0.0.10-rc-20180906_1-1_amd64
 sudo dpkg -i nv-tensorrt-repo-ubuntu1604-cuda9.0-trt5.0.0.10-rc-20180906_1-1_amd64.deb
 sudo apt-key add /var/nv-tensorrt-repo-cuda9.0-trt5.0.0.10-rc-20180906/7fa2af80.pub
 
