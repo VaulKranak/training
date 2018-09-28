0b7e6a2537b0c4397fb558c50e77421fa15e006d
vaulkranak <vaulx.kranak@intel.com>
2018-09-28 10:45:28 -0700

    fixed bash mistake

--- translation/tensorflow/run_cpu.sh
+++ translation/tensorflow/run_cpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-$IMAGE=mlperf/translation:cpu
+IMAGE=mlperf/translation:cpu
 SEED=1
 NOW=`date "+%F-%T"`
 sudo docker run -v $HOME/reference/translation/raw_data:/raw_data -it $IMAGE "./run_and_time.sh" $SEED | tee benchmark-$NOW.log
