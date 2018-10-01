46af00e2bbffabfcfdb835f4a775692c8272d1e9
vaulkranak <vaulx.kranak@intel.com>
2018-10-01 12:01:57 -0700

    added -y

--- config/specific/tensorrt/install.sh
+++ config/specific/tensorrt/install.sh
@@ -5,9 +5,9 @@ sudo dpkg -i nv-tensorrt-repo-ubuntu1604-cuda9.0-trt5.0.0.10-rc-20180906_1-1_amd
 sudo apt-key add /var/nv-tensorrt-repo-cuda9.0-trt5.0.0.10-rc-20180906/7fa2af80.pub
 
 sudo apt update
-sudo apt install tensorrt
+sudo apt install -y tensorrt
 
-sudo apt install python-libnvinfer-dev python3-libnvinfer-dev
-sudo apt install uff-converter-tf
+sudo apt install -y python-libnvinfer-dev python3-libnvinfer-dev
+sudo apt install -y uff-converter-tf
 
 dpkg -l | grep TensorRT
