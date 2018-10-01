bd4f00dad9be6102dd88f875dd4fb8fcfbaf5fda
vaulkranak <vaulx.kranak@intel.com>
2018-10-01 14:56:45 -0700

    tensorrt 5.0 not compatable

---
+++ config/cuda/cuda-9.2/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install CUDA
+sudo apt remove --purge cuda -y
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+sudo apt update
+sudo apt upgrade -y
+sudo apt install cuda-9.2 -y
+
--- config/specific/tensorrt/install.sh
+++ config/specific/tensorrt/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #sudo wget https://developer.nvidia.com/compute/machine-learning/tensorrt/5.0/rc/nv-tensorrt-repo-ubuntu1604-cuda9.0-trt5.0.0.10-rc-20180906_1-1_amd64
-sudo dpkg -i nv-tensorrt-repo-ubuntu1604-cuda9.0-trt5.0.0.10-rc-20180906_1-1_amd64.deb
-sudo apt-key add /var/nv-tensorrt-repo-cuda9.0-trt5.0.0.10-rc-20180906/7fa2af80.pub
+sudo dpkg -i nv-tensorrt-repo-ubuntu1604-cuda9.2-ga-trt4.0.1.6-20180612_1-1_amd64.deb
+#sudo apt-key add /var/nv-tensorrt-repo-cuda9.0-trt5.0.0.10-rc-20180906/7fa2af80.pub
 
 sudo apt update
 sudo apt install -y tensorrt
