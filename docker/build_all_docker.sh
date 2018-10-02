6d5099c731c9885f8e0c0d64d26ec87bee7bc5ba
vaulkranak <vaulx.kranak@intel.com>
2018-10-02 16:30:00 -0700

    fixed pip upgrade

--- docker/build_all_docker.sh
+++ docker/build_all_docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo docker build -t paddle/cpu -f paddle/cpu .
-sudo docker build -t paddle/gpu -f paddle/gpu .
-sudo docker build -t pytorch/cpu -f pytorch/cpu .
-sudo docker build -t pytorch/gpu -f pytorch/gpu .
-sudo docker build -t tensorflow/cpu -f tensorflow/cpu .
-sudo docker build -t tensorflow/gpu -f tensorflow/gpu .
+sudo docker build -t paddle/cpu -f paddle/cpu/Dockerfile .
+sudo docker build -t paddle/gpu -f paddle/gpu/Dockerfile .
+sudo docker build -t pytorch/cpu -f pytorch/cpu/Dockerfile .
+sudo docker build -t pytorch/gpu -f pytorch/gpu/Dockerfile .
+sudo docker build -t tensorflow/cpu -f tensorflow/cpu/Dockerfile .
+sudo docker build -t tensorflow/gpu -f tensorflow/gpu/Dockerfile .
