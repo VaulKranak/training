ef2a7773fdd05f1f0d9713bfa3841e0bd69cdffe
vaulkranak <vaulx.kranak@intel.com>
2018-10-05 13:16:00 -0700

    fixed and refactored

--- docker/build_all_docker.sh
+++ docker/build_all_docker.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-sudo docker build -t paddle/cpu -f paddle/cpu/Dockerfile .
-sudo nvidia-docker build -t paddle/gpu -f paddle/gpu/Dockerfile .
-sudo docker build -t pytorch/cpu -f pytorch/cpu/Dockerfile .
-sudo nvidia-docker build -t pytorch/gpu -f pytorch/gpu/Dockerfile .
-sudo docker build -t tensorflow/cpu -f tensorflow/cpu/Dockerfile .
-sudo nvidia-docker build -t tensorflow/gpu -f tensorflow/gpu/Dockerfile .
+for majorDir in */
+do
+	for minorDir in $majorDir*/
+	do
+		sudo nvidia-docker build -t $(dirname "$minorDir"Dockerfile) -f "$minorDir"Dockerfile .
+	done
+done
