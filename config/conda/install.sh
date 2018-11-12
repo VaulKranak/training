4414b196c87f2dc31e280e1ecaf5b48102551f89
Kranak, Vaul <vaulx.kranak@intel.com>
2018-11-12 11:00:21 -0800

    removed unnessary setup files

--- config/conda/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Install conda 
-wget https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh
-bash Anaconda3-5.2.0-Linux-x86_64.sh -b -p ~/anaconda
-export PATH=$PATH:~/anaconda/bin
-echo 'export PATH=$PATH:~/anaconda/bin' >> ~/.bashrc 
-
--- docker/build_all_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for majorDir in */
-do
-	for minorDir in $majorDir*/
-	do
-		sudo nvidia-docker build -t $(dirname "$minorDir"Dockerfile) -f "$minorDir"Dockerfile .
-	done
-done
